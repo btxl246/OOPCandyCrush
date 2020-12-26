b5fe14e8fc9257c8ec541e19ff502e3c3bd03427
minhnguyenvinamr <71836173+minhnguyenvinamr@users.noreply.github.com>
2020-12-27 00:21:50 +0700

    Update

--- src/code/model/Sound.java
+++ src/code/model/Sound.java
@@ -1,23 +1,7 @@
 package code.model;
 
-import java.io.File;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import java.io.*;
 
 public class Sound {
-	private File soundFile = new File("8-bit Coffin Dance.wav");
-	private AudioInputStream audioIn;
-	
-	public void playSound() {
-		try{
-			Clip clip = AudioSystem.getClip();
-			audioIn = AudioSystem.getAudioInputStream(soundFile);
-			clip.open(audioIn);
-			clip.start();
-		} catch (Exception e) {
-			System.out.print("Something wong, no file found");
-		}
-	}
+
 }
